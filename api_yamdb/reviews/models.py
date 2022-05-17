633079951a9f9e40983c708f568ee77af3999520
Дамир Шамсутдинов <d4mirshd@yandex.ru>
2022-05-17 03:40:42 +0300

    work at permissions 18-fall and 35-OK

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -4,6 +4,7 @@ from django.db.models import CheckConstraint, Q, UniqueConstraint
 from users.models import User
 
 
+
 class Genre(models.Model):
     """Модель Жанров"""
     name = models.CharField(
